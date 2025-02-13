93f5c09d5b83dd747d57228d66985c979f17b1a7
Badhia <byuneskatz@flowwest.com>
2025-02-13 10:27:52 -0800

    cleans wqx temp data

--- data-raw/processing-scripts/temperature_wqx.R
+++ data-raw/processing-scripts/temperature_wqx.R
@@ -15,8 +15,55 @@ klamath_project_board <- pins::board_s3(
   region = "us-east-1"
 )
 
-## Pulling last 10 years of temp data - Filtering to monitoring location types: River/Stream, Lake, Stream, "Lake, Reservoir, Impoundment", Reservoir, spring, estuary``
+## Pulling last 10 years of temp data - Filtering to monitoring location types: River/Stream, Lake, Stream, "Lake, Reservoir, Impoundment", Reservoir, spring, estuary
+huc_code <- "180102" # huc code for Klamath basin
 
+temp_data <- readWQPdata(huc = huc_code,                       
+                         characteristicName = "Temperature, water",
+                         startDateLo = "2014-01-01",               
+                         startDateHi = Sys.Date()) |> 
+  janitor::clean_names() 
+
+#filtering to min, max, and mean
+temp_data <- temp_data |> 
+  filter(statistical_base_code %in% c("Mean", "Maximum", "Minimum"))
+
+station_metadata <- whatWQPsites(huc = huc_code, 
+                         MonitoringLocationTypeName = "River/Stream", "Lake", "Stream",
+                         "Reservoir", "Lake, Reservoir, Impoundment",  "Spring", "Estuary") |> 
+  janitor::clean_names() 
+  
+
+# join station data with temp data
+all_temp_data <- temp_data |> left_join(station_metadata) |> 
+  glimpse()
+
+# drafting water quality data (stream, gage_name, variable_name, value, unit, statistic, date)
+data_wq <- all_temp_data |> 
+  mutate(stream = NA,
+         gage_name = monitoring_location_identifier,
+         variable_name = characteristic_name,
+         value = result_measure_value,
+         unit = result_measure_measure_unit_code,
+         statistic = statistical_base_code,
+         date = activity_start_date) |> 
+  select(stream, gage_name, variable_name, value, unit, statistic, date) |> 
+  glimpse()
+
+# drafting location data (gage_name, gage_id, agency, latitude, longitude, river_mile, huc_8, stream)
+site_data <- all_temp_data |> 
+  mutate(gage_name = monitoring_location_name,
+         gage_id = monitoring_location_identifier,
+         agency = organization_identifier,
+         latitude = as.numeric(latitude_measure),
+         longitude = as.numeric(longitude_measure),
+         river_mile = NA,
+         huc_8 = NA,
+         stream = NA) |> 
+  select(gage_name, gage_id, agency, latitude, longitude, river_mile, huc_8, stream) |> 
+  distinct(gage_id, .keep_all = TRUE) |> 
+  glimpse()
+  
 
 
 # save to s3 storage
