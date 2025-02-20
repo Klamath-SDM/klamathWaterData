19a73e0c539fa5599e08d0c1f100a64180c6c1d7
Badhia <byuneskatz@flowwest.com>
2025-02-19 17:25:35 -0800

    begins framework for pulling temp data

---
+++ temperature-data-pull.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(dplyr)
+library(dataRetrieval)
+library(tidyr)
+library(purrr)
+
+# the goal of this script is to pull temperature data from different sources and save into aws bucket 
+
+# Define aws bucket (klamath-sdm)
+
+
+# WQX data pull -----
+
+
+
+
+# USGS data pull ----
+
+
+
+# save raw data into aws bucket water-quality/data-raw/
