8aa7f840003d679e1d0e1f244dad947796cfcefe
Badhia <byuneskatz@flowwest.com>
2025-02-23 13:40:06 -0800

    adds code to pull and save usgs data

--- temperature-data-pull.R
+++ temperature-data-pull.R
@@ -5,12 +5,13 @@ library(tidyr)
 library(purrr)
 library(pins)
 
-# the goal of this script is to pull temperature data from different sources and save into aws bucket 
+# the goal of this script is to pull temperature data from different sources and save into aws bucket. Pulling last 10 years of temp data
 
 # Define aws bucket (klamath-sdm)
 
 
 ### WQX data pull -----
+# raw temperature data
 wq_data_raw <- pins::board_s3(
   bucket = "klamath-sdm",
   access_key = Sys.getenv("aws_access_key_id"),
@@ -19,20 +20,21 @@ wq_data_raw <- pins::board_s3(
   region = "us-east-1",
   prefix = "water_quality/data-raw/")
 
-## Pulling last 10 years of temp data - Filtering to monitoring location types: River/Stream, Lake, Stream, "Lake, Reservoir, Impoundment", Reservoir, spring, estuary
 huc_code <- "180102" # huc code for Klamath basin
 
+# temperature data
 wqx_temp_data <- readWQPdata(huc = huc_code,                       
                          characteristicName = "Temperature, water",
                          startDateLo = "2014-01-01",               
                          startDateHi = "2025-01-01") 
-
+# gage data
 wqx_gage_data <- whatWQPsites(huc = huc_code) 
 
 
 
 ### USGS data pull ---- TODO pull gage data
 
+# temperature data ----
 usgs_gages <- c(
   "11507500", "11510700", "11530500", "11523000", "11509500", "11509370", 
   "420741121554001", "420451121510000", "420448121503100", "420853121505500", 
@@ -64,11 +66,9 @@ for (gage in usgs_gages) {
       startDate = start_date
     ) 
     
-    # Add gage_id column to identify the gage in combined dataframe
     temp_data <- temp_data |> 
       mutate(gage_id = gage)
-    
-    # Append to the list
+  
     all_data[[gage]] <- temp_data
   }, silent = TRUE)
 }
@@ -76,8 +76,27 @@ for (gage in usgs_gages) {
 # Combine all gage data into one dataframe
 usgs_temp_data <- bind_rows(all_data)
 
+# gage data ----
+usga_gage_info_list <- list()
 
-
+# Loop through each gage to pull site metadata
+for (gage in usgs_gages) {
+  message(paste("Pulling site information for gage:", gage))
+  try({
+    site_info <- readNWISsite(gage)
+    
+    # Force `map_scale_fc` to character to ensure consistent type
+    if ("map_scale_fc" %in% colnames(site_info)) {
+      site_info$map_scale_fc <- as.character(site_info$map_scale_fc)
+    }
+    
+    # Add to the list
+    usga_gage_info_list[[gage]] <- site_info
+  }, silent = TRUE)
+}
+# Combine all site info into one dataframe
+usgs_gage_data <- bind_rows(usga_gage_info_list)
+glimpse(usgs_gage_data)
 
 
 ##### save raw data into aws bucket water-quality/data-raw/
@@ -94,5 +113,9 @@ wq_data_raw |> pins::pin_write(wqx_gage_data,
 ### USGS 
 # temp data
 wq_data_raw |> pins::pin_write(usgs_temp_data,
+                               type = "csv",
+                               title = "usgs_temperature")
+# gage data
+wq_data_raw |> pins::pin_write(usgs_gage_data,
                                type = "csv",
                                title = "usgs_temperature")
