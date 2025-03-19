4235bcd04e467dabf7f57381675963ded51b82f1
Badhia <byuneskatz@flowwest.com>
2025-03-19 11:24:59 -0700

    package files for test

---
+++ R/test.R
@@ -0,0 +1,10 @@
+#' thisisname
+#'
+#' Description of what the function does.
+#'
+#' @param x Description of parameter x.
+#' @return What the function returns.
+#' @export
+thisisname <- function(x){
+  return (x*2)
+}
