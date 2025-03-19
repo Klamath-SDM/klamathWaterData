e14ee165107ddb650e1ee9a34f60b07d8c4cbc99
Maddee Rubenson <mrubenson@flowwest.com>
2025-03-19 11:30:07 -0700

    fixing error in man/test

--- R/test.R
+++ R/test.R
@@ -1,10 +1,10 @@
-#' thisisname
+#' this is a name
 #'
 #' Description of what the function does.
 #'
 #' @param x Description of parameter x.
 #' @return What the function returns.
 #' @export
-thisisname <- function(x){
+test <- function(x){
   return (x*2)
 }
