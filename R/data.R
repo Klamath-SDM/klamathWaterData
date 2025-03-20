acd183e39d2fece78f7b92a7952e50c5c327ed47
Maddee Rubenson <mrubenson@flowwest.com>
2025-03-20 15:18:13 -0700

    updating pH

--- R/data.R
+++ R/data.R
@@ -9,3 +9,16 @@
 #' @examples
 #' temperature_data |> pillar::glimpse()
 "temperature_data"
+
+#' @name ph_gage
+#' @title pH gages in the Klamath Basin watershed (HUC8 18010204)
+#' @examples
+#' ph_gage |> pillar::glimpse()
+"ph_gage"
+
+#' @name ph_data
+#' @title Publicly available pH data in the Klamath Basin watershed (HUC8 18010204)
+#' @examples
+#' ph_data |> pillar::glimpse()
+"ph_data"
+
--- data-raw/processing-scripts/ph-process-data.R
+++ data-raw/processing-scripts/ph-process-data.R
@@ -7,8 +7,8 @@ library(pins)
 library(rivermile)
 library(sf)
 
-#notes and questions: 
-# WQX - There is one site that does not seem to be located on a stream “QVIR-SRES (Shackleford at Reservation)”. Waterbody_name function did not work 
+#notes and questions:
+# WQX - There is one site that does not seem to be located on a stream “QVIR-SRES (Shackleford at Reservation)”. Waterbody_name function did not work
 # OREGONDEQ data entries taht did not work for waterbody_name are located on a weird spot
 # USBR_WQX are on a Prairie Canal
 
@@ -21,53 +21,51 @@ library(sf)
 wq_data_board <- pins::board_s3(bucket = "klamath-sdm", region = "us-east-1")
 
 
-source(here::here('data-raw', 'processing-scripts', 'utils.R'))
-
 ### WQX ----
 # pulling raw data
 # pH data
-wqx_data_raw <- wq_data_board |> 
-  pins::pin_read("water_quality/data-raw/wqx_ph_data") |> 
-  janitor::clean_names() |> 
+wqx_data_raw <- wq_data_board |>
+  pins::pin_read("water_quality/data-raw/wqx_ph_data") |>
+  janitor::clean_names() |>
   glimpse()
 
 # GAGE data
-wqx_gage_raw <- wq_data_board |> 
-  pins::pin_read("water_quality/data-raw/wqx_gage_data") |> 
-  janitor::clean_names() |> 
+wqx_gage_raw <- wq_data_board |>
+  pins::pin_read("water_quality/data-raw/wqx_gage_data") |>
+  janitor::clean_names() |>
   filter(monitoring_location_type_name %in% c("River/Stream", "Lake", "Stream",
                                               "Reservoir", "Lake, Reservoir, Impoundment",
                                               "Spring", "Estuary")) |>
   glimpse()
 
 # JOIN - station data with pH data
-all_wqx_ph_data <- wqx_data_raw |> left_join(wqx_gage_raw, 
-                             by = c("monitoring_location_identifier", "organization_formal_name", "organization_identifier")) |> 
+all_wqx_ph_data <- wqx_data_raw |> left_join(wqx_gage_raw,
+                                             by = c("monitoring_location_identifier", "organization_formal_name", "organization_identifier")) |>
   glimpse()
 
 
 #cleaning data
-all_wqx_ph_data <- all_wqx_ph_data |> 
+all_wqx_ph_data <- all_wqx_ph_data |>
   mutate(waterbody_name = extract_waterbody(monitoring_location_name),
          waterbody_name = str_to_title(waterbody_name)) # testing function
 
-# check for naming assigned 
-all_wqx_ph_data |> 
-  select(waterbody_name, monitoring_location_name, monitoring_location_identifier) |> 
-  distinct() |> 
+# check for naming assigned
+all_wqx_ph_data |>
+  select(waterbody_name, monitoring_location_name, monitoring_location_identifier) |>
+  distinct() |>
   view()
 
-wqx_ph_data <- all_wqx_ph_data |> 
+wqx_ph_data <- all_wqx_ph_data |>
   filter(!str_detect(monitoring_location_identifier, "^USGS"), # Keep NAs & remove "USGS"
-         (is.na(activity_media_subdivision_name) | activity_media_subdivision_name == "Surface Water" )) |> 
+         (is.na(activity_media_subdivision_name) | activity_media_subdivision_name == "Surface Water" )) |>
   glimpse()
-  
-all_wqx_ph_data_clean <- wqx_ph_data |> 
-mutate(waterbody_name = case_when(
-  monitoring_location_name == "Townsends Gulch" ~ "Scott River",
+
+all_wqx_ph_data_clean <- wqx_ph_data |>
+  mutate(waterbody_name = case_when(
+    monitoring_location_name == "Townsends Gulch" ~ "Scott River",
     monitoring_location_name %in% c("HOBO at Confluence of Klamath and Trinity Rivers", "CDR and Nutrients at Saints Rest Bar") ~ "Klamath River",
-    monitoring_location_name %in% c("CDR at Red Rock", "CDR at South Boundary", 
-                                    "HOBO  A at TR_NORTON", "HOBO at North Boundary", 
+    monitoring_location_name %in% c("CDR at Red Rock", "CDR at South Boundary",
+                                    "HOBO  A at TR_NORTON", "HOBO at North Boundary",
                                     "HOBO at South Boundary", "HOBO B  at TR_NORTON") ~ "Trinity River",
     monitoring_location_identifier == "323-02-I|Paradise|R6|Fremont-Winema|Paisley" ~ "Paradise Creek",
     monitoring_location_identifier == "11NPSWRD_WQX-CRLA_WQ01" ~ "Cavern Creek",
@@ -90,36 +88,36 @@ mutate(waterbody_name = case_when(
     monitoring_location_identifier == "11NPSWRD_WQX-CRLA_WQ33" ~ "Sand Creek",
     monitoring_location_identifier == "11NPSWRD_WQX-CRLA_WQ34" ~ "Munson Creek",
     # monitoring_location_identifier == "11NPSWRD_WQX-CRLA_WQ37" ~ "? Creek",
-  str_detect(monitoring_location_name, "UPPER KLAMATH LAKE") ~ "Upper Klamath Lake", 
-  monitoring_location_identifier %in% c("KLAMATHTRIBES_WQX-KL0010", "KLAMATHTRIBES_WQX-KL0011") ~ "Agency Lake",
-    TRUE ~ waterbody_name)) |> 
-    glimpse()
-  #TODO continue to clean locations that do not have explicit name
+    str_detect(monitoring_location_name, "UPPER KLAMATH LAKE") ~ "Upper Klamath Lake",
+    monitoring_location_identifier %in% c("KLAMATHTRIBES_WQX-KL0010", "KLAMATHTRIBES_WQX-KL0011") ~ "Agency Lake",
+    TRUE ~ waterbody_name)) |>
+  glimpse()
+#TODO continue to clean locations that do not have explicit name
 # resoruce: https://www.waterqualitydata.us/provider/STORET/
 
 all_wqx_ph_data_clean |>  # check
-  select(waterbody_name, monitoring_location_name, monitoring_location_identifier) |> 
-  distinct() |>  
+  select(waterbody_name, monitoring_location_name, monitoring_location_identifier) |>
+  distinct() |>
   view()
-# All Resighini Rancheria (RREPA_WQX)gages are either at a pond or at named stream 
+# All Resighini Rancheria (RREPA_WQX)gages are either at a pond or at named stream
 # TRINITY R A LEWISTON, "TRINITY R SF A SANDY BAR NR WILL, TRINITY R A HOOPA, TRINITY R A WEITCHPEC
 # CALWR_WQX-F3109500 and CALWR_WQX-F3410000 are located at a small unknown stream
 # NARS_WQX-NWC_CA-10246 is a wetland
-all_wqx_ph_data_clean <- all_wqx_ph_data_clean |> 
-mutate(waterbody_name = case_when(
-  monitoring_location_identifier %in% c("CALWR_WQX-F4164000", "CALWR_WQX-F4410000", "CALWR_WQX-F4108000", "CALWR_WQX-F4100000")  ~ "Trinity River",
-  monitoring_location_identifier %in% c("CALWR_WQX-F3122001", "KARUKDNR_WQX-WA", "KARUKDNR_WQX-HC") ~ "Klamath River", # "KLAMATH R A ORLEANS" , "Walker Bridge", "Happy Camp"
-  monitoring_location_identifier == "NARS_WQX-CASS-1313" ~ "Digger Creek",
-  TRUE ~ waterbody_name)) |> 
+all_wqx_ph_data_clean <- all_wqx_ph_data_clean |>
+  mutate(waterbody_name = case_when(
+    monitoring_location_identifier %in% c("CALWR_WQX-F4164000", "CALWR_WQX-F4410000", "CALWR_WQX-F4108000", "CALWR_WQX-F4100000")  ~ "Trinity River",
+    monitoring_location_identifier %in% c("CALWR_WQX-F3122001", "KARUKDNR_WQX-WA", "KARUKDNR_WQX-HC") ~ "Klamath River", # "KLAMATH R A ORLEANS" , "Walker Bridge", "Happy Camp"
+    monitoring_location_identifier == "NARS_WQX-CASS-1313" ~ "Digger Creek",
+    TRUE ~ waterbody_name)) |>
   glimpse()
-  
-all_wqx_ph_data_clean |>  
-  select(waterbody_name, monitoring_location_name, monitoring_location_identifier) |> 
+
+all_wqx_ph_data_clean |>
+  select(waterbody_name, monitoring_location_name, monitoring_location_identifier) |>
   distinct() |>
   view()
 
 #### water data table ----
-ph_wqx <- all_wqx_ph_data_clean |> 
+ph_wqx <- all_wqx_ph_data_clean |>
   mutate(gage_id = monitoring_location_identifier,
          gage_name = monitoring_location_name,
          variable_name = characteristic_name,
@@ -127,13 +125,13 @@ ph_wqx <- all_wqx_ph_data_clean |>
          unit = result_measure_measure_unit_code,
          statistic = statistical_base_code, # check if we want to add this manually
          date = activity_start_date,
-         stream = waterbody_name) |> 
-  select(stream, gage_name, gage_id, variable_name, value, unit, statistic, date) |> 
+         stream = waterbody_name) |>
+  select(stream, gage_name, gage_id, variable_name, value, unit, statistic, date) |>
   glimpse()
 
 
 #### monitoring site table ----
-gage_ph_wqx_clean <- all_wqx_ph_data_clean |> 
+gage_ph_wqx_clean <- all_wqx_ph_data_clean |>
   mutate(gage_name = monitoring_location_name,
          gage_id = monitoring_location_identifier,
          agency = organization_formal_name,
@@ -141,102 +139,102 @@ gage_ph_wqx_clean <- all_wqx_ph_data_clean |>
          longitude = longitude_measure,
          river_mile = NA, #10784
          huc8 = huc_eight_digit_code,
-         stream = waterbody_name) |> 
+         stream = waterbody_name) |>
   select(gage_name, gage_id, agency, latitude, longitude, river_mile, huc8, stream) |>
-  distinct() |> 
+  distinct() |>
   gage_data_format(filter_streams = FALSE) |> glimpse()
 
-gage_ph_wqx <- rivermile::find_nearest_river_miles(gage_ph_wqx_clean) |> 
+gage_ph_wqx <- rivermile::find_nearest_river_miles(gage_ph_wqx_clean) |>
   mutate(longitude = st_coordinates(gage_ph_wqx_clean)[, 1],
-    latitude = st_coordinates(gage_ph_wqx_clean)[, 2]) |> 
-  st_drop_geometry() |> 
-  select(gage_name, gage_id, agency, latitude, longitude, river_mile, huc8, stream) |> 
+         latitude = st_coordinates(gage_ph_wqx_clean)[, 2]) |>
+  st_drop_geometry() |>
+  select(gage_name, gage_id, agency, latitude, longitude, river_mile, huc8, stream) |>
   glimpse()
 
-#### saves clean data to aws ----
-wq_processed_data <- pins::board_s3(bucket = "klamath-sdm", region = "us-east-1", prefix = "water_quality/processed-data/")
-
-# pH data
-wq_processed_data |> pins::pin_write(ph_wqx,
-                                     type = "csv",
-                                     title = "ph_processed_data_usgs")
-
-# gage data 
-wq_processed_data |> pins::pin_write(gage_ph_wqx,
-                                     type = "csv",
-                                     title = "gage_ph_processed_data")
+# #### saves clean data to aws ----
+# wq_processed_data <- pins::board_s3(bucket = "klamath-sdm", region = "us-east-1", prefix = "water_quality/processed-data/")
+#
+# # pH data
+# wq_processed_data |> pins::pin_write(ph_wqx,
+#                                      type = "csv",
+#                                      title = "ph_processed_data_usgs")
+#
+# # gage data
+# wq_processed_data |> pins::pin_write(gage_ph_wqx,
+#                                      type = "csv",
+#                                      title = "gage_ph_processed_data")
 
 
 ### USGS ----
 # pulling raw data
 # pH data
-usgs_data_raw <- wq_data_board |> 
-  pins::pin_read("water_quality/data-raw/usgs_ph_data") |> 
+usgs_data_raw <- wq_data_board |>
+  pins::pin_read("water_quality/data-raw/usgs_ph_data") |>
   janitor::clean_names() |>
   glimpse()
 
-usgs_data_raw_clean <- usgs_data_raw |> 
+usgs_data_raw_clean <- usgs_data_raw |>
   mutate(gage_id = site_no,
          date = as.Date(date),
          max_ph = x_00400_00001 ,
          min_ph = x_00400_00002,
          # mean_ph = x_00010_00003 # no mean available
-         ) |> 
+  ) |>
   select(-c(x_00400_00001 , x_00400_00001_cd, x_00400_00002, x_00400_00002_cd, date_time, tz_cd)) |>
   pivot_longer(cols = c(max_ph, min_ph),
                names_to = "statistic",
                values_to = "value",
-               values_drop_na = TRUE) |> 
+               values_drop_na = TRUE) |>
   mutate(statistic = case_when(
     statistic == "max_ph" ~ "maximum",
     statistic == "min_ph" ~ "minimum",
     # statistic == "mean_temp" ~ "mean"
-    ),
-    variable_name = "pH",
-    unit = "std unit") |> # pH standard unit
-  select(agency_cd, gage_id, date, value, statistic, variable_name, unit, site_no) |> 
+  ),
+  variable_name = "pH",
+  unit = "std unit") |> # pH standard unit
+  select(agency_cd, gage_id, date, value, statistic, variable_name, unit, site_no) |>
   glimpse()
 
 
 # GAGE data
-usgs_gage_raw <- wq_data_board |> 
-  pins::pin_read("water_quality/data-raw/usgs_gage_ph_data") |> 
-  janitor::clean_names() |> 
-  mutate(station_nm = tools::toTitleCase(tolower(station_nm))) |> 
+usgs_gage_raw <- wq_data_board |>
+  pins::pin_read("water_quality/data-raw/usgs_gage_ph_data") |>
+  janitor::clean_names() |>
+  mutate(station_nm = tools::toTitleCase(tolower(station_nm))) |>
   glimpse()
 
 # JOIN - station data with temp data
-all_usgs_ph_data_raw <- usgs_data_raw_clean |> left_join(usgs_gage_raw, by = "site_no") |> 
-  select(c(agency_cd.x, site_no, date, gage_id, statistic, value, variable_name, 
-           unit, station_nm, dec_lat_va, dec_long_va, huc_cd)) |> 
+all_usgs_ph_data_raw <- usgs_data_raw_clean |> left_join(usgs_gage_raw, by = "site_no") |>
+  select(c(agency_cd.x, site_no, date, gage_id, statistic, value, variable_name,
+           unit, station_nm, dec_lat_va, dec_long_va, huc_cd)) |>
   glimpse()
 
 #cleaning data
-all_usgs_ph_data_raw <- all_usgs_ph_data_raw |> 
+all_usgs_ph_data_raw <- all_usgs_ph_data_raw |>
   mutate(waterbody_name = extract_waterbody(station_nm)) # testing function
 
-all_usgs_ph_data_raw |> 
+all_usgs_ph_data_raw |>
   select(station_nm, waterbody_name) |> distinct() |> View()
 
-all_usgs_ph_data_raw <- all_usgs_ph_data_raw |> 
-mutate(waterbody_name = case_when(station_nm %in% c("Upper Klamath Lake at Howard Bay, or", "Mid-Trench - Lower   -  Mdtl",
-                                                    "Mid-Trench - Upper   - Mdtu", "Mid-North - Lower  - Mdnl", 
-                                                    "Mid-North - Upper  - Mdnu", "Rattlesnake Point  -  Rpt",
-                                                    "Fish Banks West - Fbw") ~ "Upper Klamath Lake",
-                                  station_nm == "Shoalwater Bay - Shb" ~ "Shoalwater Bay",
-                                  T ~ waterbody_name)) |> 
+all_usgs_ph_data_raw <- all_usgs_ph_data_raw |>
+  mutate(waterbody_name = case_when(station_nm %in% c("Upper Klamath Lake at Howard Bay, or", "Mid-Trench - Lower   -  Mdtl",
+                                                      "Mid-Trench - Upper   - Mdtu", "Mid-North - Lower  - Mdnl",
+                                                      "Mid-North - Upper  - Mdnu", "Rattlesnake Point  -  Rpt",
+                                                      "Fish Banks West - Fbw") ~ "Upper Klamath Lake",
+                                    station_nm == "Shoalwater Bay - Shb" ~ "Shoalwater Bay",
+                                    T ~ waterbody_name)) |>
   glimpse()
 
 #### water data table ----
-ph_usgs <- all_usgs_ph_data_raw |> 
+ph_usgs <- all_usgs_ph_data_raw |>
   mutate(gage_id = site_no,
          gage_name = station_nm,
-         stream = waterbody_name) |> 
-  select(stream, gage_name, gage_id, variable_name, value, unit, statistic, date) |> 
+         stream = waterbody_name) |>
+  select(stream, gage_name, gage_id, variable_name, value, unit, statistic, date) |>
   glimpse()
 
 #### monitoring site table ----
-gage_ph_usgs_clean <- all_usgs_ph_data_raw |> 
+gage_ph_usgs_clean <- all_usgs_ph_data_raw |>
   mutate(gage_name = station_nm,
          gage_id = site_no,
          agency = agency_cd.x,
@@ -244,28 +242,48 @@ gage_ph_usgs_clean <- all_usgs_ph_data_raw |>
          longitude = dec_long_va,
          river_mile = NA,
          huc8 = huc_cd,
-         stream = waterbody_name) |> 
-  select(gage_name, gage_id, agency, latitude, longitude, river_mile, huc8, stream) |> 
-  distinct() |> 
-  gage_data_format(filter_streams = FALSE) |> 
+         stream = waterbody_name) |>
+  select(gage_name, gage_id, agency, latitude, longitude, river_mile, huc8, stream) |>
+  distinct() |>
+  gage_data_format(filter_streams = FALSE) |>
   glimpse()
 
 gage_ph_usgs <- rivermile::find_nearest_river_miles(gage_ph_usgs_clean) |>
   mutate(longitude = st_coordinates(gage_ph_usgs_clean)[, 1],
          latitude = st_coordinates(gage_ph_usgs_clean)[, 2]) |>
   st_drop_geometry() |>
-  select(gage_name, gage_id, agency, latitude, longitude, river_mile, huc8, stream) |> 
+  select(gage_name, gage_id, agency, latitude, longitude, river_mile, huc8, stream) |>
   glimpse()
 
 
-### saves clean data to aws 
+# combine wqx and usgs pH data  -------------------------------------------
+ph_data <- ph_wqx |>
+  mutate(variable_name = "pH",
+         statistic = tolower(statistic),
+         unit = "std unit", # note that wqx was previouly none, "std unit" comes from USGS conventions
+         value = as.numeric(value),
+         date = as.Date(date)) |>
+  bind_rows(ph_usgs |>
+              mutate(gage_id = as.character(gage_id))) |>
+  #remove two NAs in value
+  filter(!is.na(value))
 
-# temp data
-wq_processed_data |> pins::pin_write(ph_usgs,
-                                     type = "csv",
-                                     title = "ph_processed_data_usgs")
+ph_gage <- gage_ph_usgs |>
+  mutate(gage_id = as.character(gage_id)) |>
+  bind_rows(gage_ph_wqx) |>
+  glimpse()
 
-# gage data 
-wq_processed_data |> pins::pin_write(gage_ph_usgs,
-                                     type = "csv",
-                                     title = "ph_gage_processed_data")
+### saves clean data to aws
+
+# # pH data
+wq_processed_data |> pins::pin_write(ph_data,
+                                     type = "csv")
+
+# gage data
+wq_processed_data |> pins::pin_write(ph_gage,
+                                     type = "csv")
+
+# save rda files
+usethis::use_data(ph_data, overwrite = TRUE)
+usethis::use_data(ph_gage, overwrite = TRUE)
+
