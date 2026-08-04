fd6add8221677b58c3daf09d93ec123e50116746
Badhia <byuneskatz@flowwest.com>
2026-08-04 11:02:41 -0700

    fix numeric column, writes creak flow gage file

--- data-raw/processing-scripts/flow-process-data.R
+++ data-raw/processing-scripts/flow-process-data.R
@@ -245,7 +245,8 @@ flow_gage <- gage_flow_wqx |>
   # These two sites didn't overlap the NHD line for the Scott River so marking their RM as NA
   mutate(river_mile = case_when(gage_name == "Scott River at Gaging Station" ~ NA,
                                 gage_name == "Scott River South Fork" ~ NA,
-                                .default = as.numeric(river_mile))) |>
+                                .default = as.numeric(river_mile)),
+         huc8 = as.numeric(huc8)) |>
   mutate(location = tolower(stream)) |>
   relocate(location, .before = gage_name) |>
   select(-stream) |>
